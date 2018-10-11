58c354a5a8dabb1efc0df8bbd3491fe8096a7089
ZaheerAlvi <zaheermalik284@gmail.com>
2018-10-11 10:15:31 +0500

    comment is added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import comments from './components/comments/comments'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props){
+        super(props);
+        this.state={
+            isloaded: true,
+            iscloaded: false,
+            items:[],
+            images:[]
+        }
+
+    }
+
+    componentDidMount(){
+
+        fetch('https://dog.ceo/api/breeds/list/all')
+
+            .then(res => res.json())
+            .then(res2 => {
+                this.setState({items : res2.message});
+            });
+
+
+
+    }
+
+        showimagesHandler=(props)=>{
+            const breed =props.
+            fetch('https://dog.ceo/api/breeds/images')
+                .then(res => res.json())
+                .then(json => {
+                    console.log(json.message);
+                    this.setState({
+                        isloaded:true,
+                        images : json.message
+                    })
+                });
+        }
+
+    render(){
+        var {isloaded , items ,images}=this.state;
+        var id;
+
+        if (this.state.items) {
+            return (
+                <div>
+                    <ul>
+                        {
+                            Object.keys(items).map((key)=>(
+                              <li><a onClick={this.showimagesHandler.bind(that, key)} href={key}>{key}</a></li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            )
+        }
+
+
+
+        if(isloaded==true) {
+            return (
+                <div id="root" className="App">
+                    <div>
+                        <h2>
+                            {
+                                // JSON.stringify(images)
+                                // JSON.stringify(this.state.items)
+                            }
+                        </h2>
+
+                    </div>
+                </div>
+            );
+        }else {
+            return <h2>Loading......</h2>;
+        }
+    }
 }
 
 export default App;
---
+++ src/components/comments/comments.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const comments=(props)=>{
+    /*render(){
+        return(
+            <div className="posts">
+                {comments.map(comment =>(
+                    <div id={comment.id} key={comment.id} className="post">
+                        <h4 className="title">{comment.name}</h4>
+                        <p className="body">{comment.body}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }*/
+}
+
+export default comments;
